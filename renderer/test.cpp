63c872e5e8d3e0e7044058f2e4212e65cf77eb85
abhiraj2 <abhiraj.official15@gmail.com>
2022-02-01 18:54:41 +0530

    Triangles!! Ye

--- renderer/test.cpp
+++ renderer/test.cpp
@@ -8,6 +8,7 @@
 
 const TGAColor white = TGAColor(255,255,255,255);
 const TGAColor red = TGAColor(255,0,0,255);
+const TGAColor green = TGAColor(0,255,0,255);
 const int width = 800;
 const int height = 800;
 Model *model = NULL;
@@ -45,15 +46,45 @@ void draw_line(TGAImage& image, int x0, int y0, int x1, int y1, TGAColor color){
 	}
 }
 
-int main(int argc, char** argv){
-	if(2==argc){
-		model = new Model(argv[1]);
-	}
-	else {
-		model = new Model("head.obj");
+
+void triangle(TGAImage& image, Vec2i t0, Vec2i t1, Vec2i t2, TGAColor color){
+	if(t0.y > t1.y) std::swap(t0, t1);
+	if(t1.y > t2.y) std::swap(t1, t2);
+	if(t0.y > t2.y) std::swap(t0, t2);
+	int height = t2.y - t0.y;
+	
+	for(int y = t0.y; y <=t1.y; y++){
+		int segment = (t1.y-t0.y+1);
+		float alpha = (float)(y-t0.y)/(float)height; //interpolate the largest side from y0 to y1
+		float beta = (float)(y-t0.y)/(float)segment; // other side from y0 to y1
+		
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = t0 + (t1-t0)*beta;
+		if(A.x > B.x) std::swap(A, B); // x-coord on the largest side to x-coord at the other side
+		for(int j = A.x; j<=B.x; j++){ //draw from a.x to b.x per y
+			image.set(j, y, color); 
+		}
+		
 	}
 	
-	TGAImage image(width, height, TGAImage::RGB);
+	for(int y = t1.y; y <=t2.y; y++){ //same shit for upper triangle
+		int segment = (t2.y-t1.y+1);
+		float alpha = (float)(y-t0.y)/(float)height; //interpolate the largest side from y1 to y2
+		float beta = (float)(y-t1.y)/(float)segment; //The other sides from y1 to y2
+		
+		Vec2i A = t0 + (t2-t0)*alpha; //you still go from each vertex of A from 0
+		Vec2i B = t1 + (t2-t1)*beta; // Already drew up until t1 so start from t1.
+		if(A.x > B.x) std::swap(A, B);
+		for(int j = A.x; j<=B.x; j++){
+			image.set(j, y, color);
+		}
+		
+	}
+	
+}
+
+
+void draw_wire_frame(Model* model, TGAImage& image){
 	for(int i=0; i< model->nfaces(); i++){
 		std::vector<int> face = model -> face(i);
 		for(int j=0; j<3; j++){
@@ -66,8 +97,28 @@ int main(int argc, char** argv){
 			draw_line(image, x0, y0, x1, y1, white);
 		}
 	}
+}
+
+int main(int argc, char** argv){
+	/*
+	if(2==argc){
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("head.obj");
+	}*/
+	
+	TGAImage image(width, height, TGAImage::RGB);
+	
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+	Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+	triangle(image, t0[0], t0[1], t0[2], red);
+	triangle(image, t1[0], t1[1], t1[2], white);
+	triangle(image, t2[0], t2[1], t2[2], green);
+	
 	//image.flip_vertically();
 	image.write_tga_file("out.tga");
-	delete model;
+	//delete model;
 	return 0;
 }
